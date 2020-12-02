9861046e1224958a4531071802e3a61af61d78cc
Postmaxxx <71179917+Postmaxxx@users.noreply.github.com>
2020-12-02 15:26:57 +0800

    feature(АвтоматическоеТестирование): first variant of tests
    New short command added: "npm run c" (add + cz + push)

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,32 @@
+const reverse = require('../index.js');
+
+
+describe('Tests for reverse', () => {
+    const string1 = `abc`;
+    const string2 = `abcdEfg`;
+    const string3 = `abcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefg`;
+    const string4 = `abcdefg5f`;
+    const string5 = `abcdefg.f`;
+    const string6 = `abcdefgЖf`;
+
+    it('Short string', () => {
+        expect(reverse(string1)).toBe(`String too short! (${string1.length} symbols)`)
+    });
+    it('Normal string', () => {
+        expect(reverse(string2)).toBe(`gfEdcba`)
+    });
+    it('Long string', () => {
+        expect(reverse(string3)).toBe(`String too long! (${string3.length - 20} symbols excess)`)
+    });
+
+    it('Invalid symbols string', () => {
+        expect(reverse(string4)).toBe(`String contains ivalid symbols!`)
+    });
+    it('Invalid symbols string', () => {
+        expect(reverse(string5)).toBe(`String contains ivalid symbols!`)
+    });
+    it('Invalid symbols string', () => {
+        expect(reverse(string6)).toBe(`String contains ivalid symbols!`)
+    });
+
+})
--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import ('./styles/main.css');
+
+let input = document.getElementById('input1');
+let button = document.getElementById('button1');
+let result = document.getElementById('result1');
+
+
+
+
+
+
+
+
+function reverse(string) {
+    if (string.length < 5 ) return `String too short! (${string.length} symbols)`;
+    if (string.length > 20 ) return `String too long! (${string.length - 20} symbols excess)`;
+    if (string != string.match(/[A-Za-z]*/)[0]) return "String contains ivalid symbols!"
+    return string.split('').reverse().join('');
+};
+
+
+function showResult(text) {
+    result.innerText = text;
+    console.log(text);
+};
+
+
+
+function pageLoaded() {
+    button.addEventListener('click', () => {
+        showResult(reverse(input.value));
+    })
+};
+
+
+
+document.addEventListener("DOMContentLoaded", pageLoaded);
+
+module.exports = reverse;
